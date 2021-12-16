681f4476a6e62c2f41caf2ae505dfb02912fcb5b
SamZayd <sadafk2304@gmail.com>
2021-12-16 14:55:08 +0530

    commit

---
+++ Skill test/JS Beginner assignment/FatArrow.js
@@ -0,0 +1,9 @@
+//Fat Arrow Function
+let addac = (a,b)=> a+b;
+result=addac(2,5)
+console.log("Addition",result);
+
+let accab =(a,b)=>{
+    console.log("Addition",a+b);
+}
+accab(3,6)
---
+++ Skill test/JS Beginner assignment/arrays.js
@@ -0,0 +1,7 @@
+let array = [10,true,"Ten",{id:101,name:"sam"}]
+console.log(typeof array[1]);
+console.log(array);
+i=0
+while(i<=array.length-1){
+    console.log(i[array]);
+}
