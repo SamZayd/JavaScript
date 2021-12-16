836cae76e2ee71c1dcb43d34f8d4ca5e609d6107
SamZayd <sadafk2304@gmail.com>
2021-12-16 22:58:37 +0530

    go

---
+++ Skill test/JS Beginner assignment/array-2D.js
@@ -0,0 +1,11 @@
+array =[[1,2,3],
+        [4,5,6],
+        [7,8,9]];
+
+console.log(array);
+
+console.log(array[0][0],array[1][1],array[2][2]);
+
+//update element
+array[2][1]=80;
+console.log(array);
--- Skill test/JS Beginner assignment/arrays.js
+++ Skill test/JS Beginner assignment/arrays.js
@@ -1,7 +1,81 @@
-let array = [10,true,"Ten",{id:101,name:"sam"}]
-console.log(typeof array[1]);
+let array = [10,true,"Ten",{id:101,name:"sam"},false,101.22]
+
+console.log("Type of third element :",typeof array[3]);
+
 console.log(array);
-i=0
+
+let i=0
+console.log("Using while loop");
 while(i<=array.length-1){
-    console.log(i[array]);
+    console.log(i,".",array[i]);
+    i++;
 }
+
+console.log("Using for loop");
+for(i=0; i<=array.length-1; i++){
+    console.log(i,".",array[i]);
+}
+
+//Array object
+console.log("\nArray with object\n");
+let emp = [{id: 100,name: "Sam"},{id:101,name: "Zayd"}];
+console.log(emp);
+
+let myarr=[];
+myarr[0]=Date.now;
+myarr[1]=emp;
+myarr[2]=array;
+console.log(myarr);
+console.log(myarr[0]);
+
+//Array methods
+console.log("\nMethods Here\n");
+
+console.log("Array is ",array);
+
+array.sort()
+console.log("sort ",array);
+
+array.shift()       //remove first element 
+console.log("shift ",array);
+
+array.unshift("Two")    //add element to start
+console.log("unshift ",array);
+
+array.pop()         //remove last element
+console.log("pop ",array);
+
+array.push("SamZayd")       //add element to last
+console.log("push ",array);
+
+console.log("Array is ",array);
+
+array.splice(2,2)   //start from 2 & remove 2 elements
+console.log("splice ",array);
+
+delete array[2]
+console.log("After Delete [2] ",array);
+
+let any=['It','s',8,'o','clock',true]
+
+console.log("any =",any);
+
+console.log("toString ",any.toString());
+
+console.log("join ",any.join(" "));
+
+//Concatination
+
+console.log("\nConcat array with any\n");
+
+console.log("Array =",array);
+console.log("Any =",any);
+
+arrayany=array.concat(any);     //concat of two array
+console.log("Array+any =",arrayany);
+
+all=arrayany.concat(myarr,emp)
+console.log("Arrayany+myarr+any =",all);
+
+all.sort()
+console.log("Sort all =",all);
